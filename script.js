34eb0a048911844e35971991a713974119dce88b
dewalt-1 <benodru@gmail.com>
2024-07-08 17:36:18 +0200

    added location and destination route

--- script.js
+++ script.js
@@ -1,25 +1,81 @@
-function getRoute(end) {
-    var start = [currentLocation.longitude, currentLocation.latitude];
-    var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start.join(',') + ';' + end.join(',') +
-        '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuMTExMTExMSIsImEiOiJjazhocjVwenMwMnh5M2twYzl6cngwNW92In0.RKO2gVTlIs3FYXmoKrzVYQ'; // Replace this with your Mapbox Access Token
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        var route = data.routes[0].geometry;
-        var geojson = {
-            type: 'Feature',
-            properties: {},
-            geometry: route
-        };
-        // Assuming a map layer is already defined to display the route
-        map.getSource('route').setData(geojson);
-        // Display step-by-step directions
-        var steps = data.routes[0].legs[0].steps;
-        var tripInstructions = document.getElementById('instructions');
-        tripInstructions.innerHTML = '';
-        steps.forEach((step, i) => {
-            tripInstructions.innerHTML += '<li>' + step.maneuver.instruction + '</li>';
-        });
+// Function to initialize the map at the user's current location
+function loadMap(initialCoordinates) {
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: initialCoordinates, // Use the user's current location as the initial center
+        zoom: 14
     });
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+    window.map = map; // Make map globally available
+}
+
+// Get user's current location
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true });
+
+function successLocation(position) {
+    loadMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+    loadMap([-74.5, 40]); // Default location if geolocation fails
+}
+
+function submitDestination() {
+    var destinationInput = document.getElementById('destination').value;
+    if (destinationInput === "") {
+        alert("Please enter a destination.");
+        return;
+    }
+
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(destinationInput)}.json?access_token=${mapboxgl.accessToken}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.features.length === 0) {
+                alert("Destination not found.");
+                return;
+            }
+            var destinationCoords = data.features[0].center;
+            getRoute([window.map.getCenter().lng, window.map.getCenter().lat], destinationCoords);
+        });
+}
+
+function getRoute(start, end) {
+    var url = `https://api.mapbox.com/directions/v5/mapbox/driving/${start.join(',')};${end.join(',')}?geometries=geojson&steps=true&access_token=${mapboxgl.accessToken}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var route = data.routes[0].geometry;
+            var routeLayer = {
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: route
+                    }
+                },
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                paint: {
+                    'line-color': '#3887be',
+                    'line-width': 5,
+                    'line-opacity': 0.75
+                }
+            };
+            // Add or update the route on the map
+            if (map.getSource('route')) {
+                map.getSource('route').setData(route);
+            } else {
+                map.addLayer(routeLayer);
+            }
+        });
 }
